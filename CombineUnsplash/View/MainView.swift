3be66a26c8484d8933bd8bc0fcf2874e5af4feed
Vinh Nguyen <vinhnguyen2308@gmail.com>
2019-06-09 05:33:43 +0700

    Update note on @State

--- CombineUnsplash/View/MainView.swift
+++ CombineUnsplash/View/MainView.swift
@@ -16,21 +16,26 @@ struct MainView: View {
     // A `Publisher` view model that taking care of fetching remote API on `TextField` enter key
     // press event emits. It will publish data event for any `Subscriber` to populate data when the
     // network response comes in
-    @EnvironmentObject internal var viewModel: SplashViewModel
+    @EnvironmentObject private var viewModel: SplashViewModel
 
-    // We just want `category` as view state for internal observers
-    @State internal var category = ""
+    // @State `category`, because we will use it for both read (#1) and write (#2).
+    // IMPORTANT: @State properites should be declared as `private`
+    // to prevent other views mutate it.
+    @State private var category = ""
 
     // MARK: - View
 
     var body: some View {
         VStack(alignment: .center, spacing: 30) {
             HStack {
-                TextField($category, placeholder: Text("eg: nature, then press enter")) {
+                // #2 write (mutates) the underlying value of `category`
+                TextField($category, placeholder: Text("eg: nature")) {
                     self.fetchData()
                 }.font(.body)
 
                 VStack {
+                    // #1 read the value of `category`
+                    // you can just use `category` to read the underlying value of $category
                     Text($category.value.uppercased())
                         .font(.title)
                     Text("from Unsplash.com")
@@ -51,6 +56,8 @@ struct MainView: View {
     // MARK: - Private
 
     private func fetchData() {
+        // like #1 above
+        // you can just use `category` to read the underlying value of $category
         self.viewModel.fetch($category.value)
     }
 }
